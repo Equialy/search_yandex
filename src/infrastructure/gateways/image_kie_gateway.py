2dd782ea986860b25dcf5d436e9baf058f1fbba5
ips_aa <ips_aa@gmail.com>
2026-09-08 10:33:44 +0300

    fix: increase timeout for await image heneration

--- src/infrastructure/gateways/image_kie_gateway.py
+++ src/infrastructure/gateways/image_kie_gateway.py
@@ -67,7 +67,7 @@ class ImageKieGenerationGateway:
             raise ValueError(f"Task creation failed: {res.text}")
 
         result_img_url = None
-        for _ in range(45):
+        for _ in range(90):
             await asyncio.sleep(2.0)
             poll_res = await self._http.get(
                 f"{self._base_url}/api/v1/jobs/recordInfo",
